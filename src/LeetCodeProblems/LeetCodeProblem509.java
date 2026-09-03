072e8faf8c839486337d457a822b51112e06b8f1
Rishu Garg <rishu63318@gmail.com>
2026-09-03 14:47:53 +0530

    Add solution for LeetCode 509 Fibonacci Number

--- src/LeetCodeProblems/LeetCodeProblem509.java
+++ src/LeetCodeProblems/LeetCodeProblem509.java
@@ -1,4 +1,10 @@
 package LeetCodeProblems;
-
+// LeetCode Problem 509 = Fibonacci Number
 public class LeetCodeProblem509 {
+    public int fib(int n) {
+        if(n==0||n==1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
 }
