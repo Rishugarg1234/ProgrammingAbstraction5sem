5612513bd72e42237f9e7ffd84b2a0948bd87913
Rishu Garg <rishu63318@gmail.com>
2026-09-10 17:55:05 +0530

    Add solution for LeetCode 1721 Swapping Nodes in a Linked List

---
+++ src/LeetCodeProblems/LeetCodeProblem1721.java
@@ -0,0 +1,27 @@
+package LeetCodeProblems;
+// LeetCode Problem 1721  Swapping Nodes in a Linked List
+public class LeetCodeProblem1721 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode first = head;
+        for (int i=1;i<k;i++){
+            first = first.next;
+        }
+        ListNode second = head;
+        ListNode temp = first;
+        while(temp.next!=null){
+            temp = temp.next;
+            second = second.next;
+        }
+        int v = first.val;
+        first.val = second.val;
+        second.val = v;
+        return head;
+    }
+}
